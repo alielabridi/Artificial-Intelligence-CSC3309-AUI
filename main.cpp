e166827e983cff76ca3c7b7397cddd6e76cb9c31
alielabridi <elabridiali@outlook.com>
2016-10-29 17:39:53 +0100

    depth order

--- main.cpp
+++ main.cpp
@@ -33,20 +33,21 @@ int main(int argc, char *argv[]){
     getline(problemSpecification, problem);
     getline(problemSpecification,strategy);
 
+    //look for the depth cutoff in case of IDA DLS IDS
+    if(strategy == "IDS " || strategy == "IDA" || strategy == "DLS")
+        problemSpecification >> depth_cutoff;
+
     if(problem == "Pegs")//for the pegs
         problemSpecification >> pegs_shape;
 
         //MCP
     else if(problem == "MCP"){
         problemSpecification >> n_missionaries;
-        problemSpecification >> n_cannibals;
+        n_cannibals = n_missionaries;
         problemSpecification >> n_people;
     }
     problemSpecification >> n_nodes_to_expand;
 
-    //look for the depth cutoff in case of IDA DLS IDS
-    if(strategy == "IDS " || strategy == "IDA" || strategy == "DLS")
-        problemSpecification >> depth_cutoff;
 
     if(VERBOSE){
         cout << "Problem " << problem  << endl;
