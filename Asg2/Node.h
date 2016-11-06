c625f1016e70c8b675d0fc37ac91648ad9792ad8
alielabridi <elabridiali@outlook.com>
2016-11-06 01:45:30 +0000

    changes before going to master

--- Asg2/Node.h
+++ Asg2/Node.h
@@ -14,10 +14,12 @@ class Node {
     /*prev next */
 public:
     Node *prev;
+    int pathcode = 0;
     virtual bool equals(Node* node)=0;
     virtual void printState()=0;
     virtual bool goalStateTest()=0;
     virtual vector<Node*> successorFunction()=0;
+    virtual int stepCost(int move)=0;
 };
 
 #endif //ASG2_NODE_H
--- Asg2/PegsLogicNode.h
+++ Asg2/PegsLogicNode.h
@@ -14,24 +14,76 @@ class NodePegs : public Node {
 public:
     vector<vector<char> > state;
     int size;
+    int HeuristicValue =0; /*based on the total number of moves possible in a given state*/
 
-    char getValueState(int i, int j){
-        return state[i][j];
-    }
-    int getSize(){return size;}
-    
+
     NodePegs() {};
 
     NodePegs(int size,vector<vector<char> > state_):state(size, std::vector<char>(size)) {
         this->size = size;
         /*copy matrix*/
         state = state_;
+        HeuristicValue = totalPossibleMovesHeuristic();
+    }
+
+
+
+
+int stepCost(int move) {
+    /*returns the step cost for every move
+     * 1 => down
+     * 2 =>right
+     * 2 => left
+     * 3 => up
+     * */
+    switch (move) {
+        case 1:
+            return 1;
+        case 2:
+            return 1;
+        case 3:
+            return 1;
+        case 4:
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+    int totalPossibleMovesHeuristic(){
+        int count = 0;
+        for (int i = 0; i < size ; ++i) {
+            for (int j = 0; j < size ; ++j) {
+                if (state[i][j] == '1') {
+
+                    /*check possibility to move blank down*/
+                    if ((i + 2 < size) && state[i + 1][j] == '1' && state[i + 2][j] == '0') {
+                        count++;
+                    }
+
+                    /*check possibility to move blank right*/
+                    if ((j + 2 < size) && state[i][j + 1] == '1' && state[i][j + 2] == '0') {
+                        count++;
+                    }
+                    /*check possibility to move blank left*/
+                    if ((j - 2 >= 0) && state[i][j - 1] == '1' && state[i][j - 2] == '0') {
+                        count++;
+                    }
+                    /*check possibility to move blank up*/
+                    if ((i - 2 >= 0) && state[i - 1][j] == '1' && state[i - 2][j] == '0') {
+                        count++;
+                    }
+
+                }
+            }
+        }
+        return count;
     }
 
     bool equals(Node* node){
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                if (node->getValueState(i,j) != state[i][j])
+                if (dynamic_cast<NodePegs *>(node)->state[i][j] != state[i][j])
                     return false;
             }
         }
@@ -54,7 +106,7 @@ public:
         /*to be modified for the array goalstate*/
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                if (goalStatePegs4[i][j] != state[i][j])
+                if (goalStatePegs[i][j] != state[i][j])
                     return false;
             }
         }
@@ -74,6 +126,7 @@ public:
                         nodeDown->state[i + 2][j] = '0';
                         nodeDown->state[i][j] = '1';
                         nodeDown->prev = this;
+                        nodeDown->pathcode = pathcode + stepCost(1);
                         successorSet.push_back(nodeDown);
                     }
 
@@ -84,6 +137,7 @@ public:
                         nodeRight->state[i][j + 2] = '0';
                         nodeRight->state[i][j] = '1';
                         nodeRight->prev = this;
+                        nodeRight->pathcode = pathcode + stepCost(2);
                         successorSet.push_back(nodeRight);
                     }
                     /*check possibility to move blank left*/
@@ -93,6 +147,7 @@ public:
                         nodeLeft->state[i][j - 2] = '0';
                         nodeLeft->state[i][j] = '1';
                         nodeLeft->prev = this;
+                        nodeLeft->pathcode = pathcode +  stepCost(3);
                         successorSet.push_back(nodeLeft);
                     }
                     /*check possibility to move blank up*/
@@ -102,6 +157,7 @@ public:
                         nodeUp->state[i - 2][j] = '0';
                         nodeUp->state[i][j] = '1';
                         nodeUp->prev = this;
+                        nodeUp->pathcode = pathcode +  stepCost(4);
                         successorSet.push_back(nodeUp);
                     }
 
